538fe8804ad94662ef805c74f43953f68b6ebd66
AustinFlagler <austindflagler@hotmail.com>
2017-08-07 16:27:58 -0400

    initial commit
    test 2

---
+++ forms.py
@@ -0,0 +1,37 @@
+from flask_wtf import Form
+from wtforms import (StringField, PasswordField)
+from wtforms.validators import (DataRequired, Email, Length, EqualTo,
+                                ValidationError)
+
+from models import User
+
+
+def email_exists(form, field):
+    if User.select().where(User.email == field.data).exists():
+        raise ValidationError('User with that email already exists')
+
+
+class RegisterForm(Form):
+    email = StringField(
+        'Email',
+        validators =[
+            DataRequired(),
+            Email(),
+            email_exists
+        ])
+    password = PasswordField(
+        'Password',
+        validators=[
+            DataRequired(),
+            Length(min=2),
+            EqualTo('password2', message='Passwords must match')
+        ])
+    password2 = PasswordField(
+        'Confirm Password',
+        validators=[DataRequired()]
+    )
+
+
+class LoginForm(Form):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
