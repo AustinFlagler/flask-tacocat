b0c467a84dc9eb832f565481d77f0c5822df317f
AustinFlagler <30806774+AustinFlagler@users.noreply.github.com>
2017-08-07 16:33:33 -0400

    Update forms.py

--- forms.py
+++ forms.py
@@ -1,37 +1,40 @@
 from flask_wtf import Form
-from wtforms import (StringField, PasswordField)
-from wtforms.validators import (DataRequired, Email, Length, EqualTo,
-                                ValidationError)
+from wtforms import StringField, PasswordField, BooleanField, TextAreaField
+from wtforms.validators import ValidationError, DataRequired, Email, Length, EqualTo
 
 from models import User
 
+import logging
 
 def email_exists(form, field):
+    logging.debug(User.email)
     if User.select().where(User.email == field.data).exists():
-        raise ValidationError('User with that email already exists')
-
+        raise ValidationError('User with that email already exists.')
 
 class RegisterForm(Form):
-    email = StringField(
-        'Email',
-        validators =[
-            DataRequired(),
-            Email(),
-            email_exists
-        ])
-    password = PasswordField(
-        'Password',
-        validators=[
-            DataRequired(),
-            Length(min=2),
-            EqualTo('password2', message='Passwords must match')
-        ])
-    password2 = PasswordField(
-        'Confirm Password',
-        validators=[DataRequired()]
-    )
-
+    email = StringField('Email',
+                        validators=[
+                                DataRequired(),
+                                Email(),
+                                email_exists
+                            ])
+    password = PasswordField('Password',
+                             validators=[
+                                DataRequired(),
+                                Length(min=1),
+                                EqualTo('password2', message='Password must match.')
+                            ])
+    password2 = PasswordField('Confirm Password',
+                              validators=[
+                                DataRequired()
+                            ])
 
 class LoginForm(Form):
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
+
+class TacoForm(Form):
+    protein = StringField('Protein', validators=[DataRequired()])
+    shell = StringField('Shell', validators=[DataRequired()])
+    cheese = BooleanField('Cheese', validators=[DataRequired()])
+    extras = TextAreaField('Extras', validators=[DataRequired()])
