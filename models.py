c2c8993c8bec6c64af4d7b592069f9aeeeba4db2
AustinFlagler <30806774+AustinFlagler@users.noreply.github.com>
2017-08-07 16:31:13 -0400

    Update models.py

--- models.py
+++ models.py
@@ -1,34 +1,43 @@
 import datetime
 
-from flask.ext.bcrypt import generate_password_hash
 from flask.ext.login import UserMixin
+from flask.ext.bcrypt import generate_password_hash
 from peewee import *
 
-DATABASE = SqliteDatabase('tacocat.db')
-
+DATABASE = SqliteDatabase('tacos.db')
 
 class User(UserMixin, Model):
-    email = CharField(unique=True)
-    password = CharField(max_length=100)
-    joined_at = DateTimeField(default=datetime.datetime.now)
+    email = CharField(unique = True)
+    password = CharField(max_length = 100)
 
     class Meta:
         database = DATABASE
-        order_by = ('-joined_at',)
 
+    def get_tacos(self):
+        return Taco.select().where(Taco.user == self)
 
     @classmethod
     def create_user(cls, email, password):
         try:
-            cls.create(
-                email=email,
-                password=generate_password_hash(password),
-            )
+            with DATABASE.transaction():
+                cls.create(
+                    email = email,
+                    password = generate_password_hash(password)
+                )
         except IntegrityError:
-            raise ValueError("That user already exists.")
+            raise ValueError("Sorry, that user already exists.")
 
+class Taco(Model):
+    user = ForeignKeyField(rel_model = User, related_name = "tacos")
+    protein = CharField()
+    shell = CharField()
+    cheese = BooleanField()
+    extras = TextField()
+
+    class Meta:
+        database = DATABASE
 
 def initialize():
     DATABASE.connect()
-    DATABASE.create_tables([User], safe=True)
+    DATABASE.create_tables([User, Taco], safe=True)
     DATABASE.close()
